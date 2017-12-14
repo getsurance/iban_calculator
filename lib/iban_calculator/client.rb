7c893318b268f4dd88cc4964a9829a169e3fd66a
Christian Georgii <cgeorgii@gmail.com>
2017-12-14 16:23:59 +0100

    do not raise error for failed result

--- lib/iban_calculator/client.rb
+++ lib/iban_calculator/client.rb
@@ -3,8 +3,6 @@ require_relative 'response'
 
 module IbanCalculator
   class Client
-    VALID_RETURN_CODE = 0..31
-    PROBABLY_VALID_RETURN_CODE = 32..127
     SERVICE_ERROR_RETURN_CODE = 65536
 
     def initialize(user:, password:, adapter_options: {})
@@ -15,20 +13,9 @@ module IbanCalculator
 
     def call(operation, payload = {})
       raw_response = adapter.(operation, message: payload.merge(credentials))
-      old_response = raw_response.body["#{operation}_response".to_sym][:return]
+      response = Response.new(raw_response.body["#{operation}_response".to_sym][:return])
 
-      response = Response.new(old_response)
-
-      case response.return_code
-      when VALID_RETURN_CODE
-        response
-      when PROBABLY_VALID_RETURN_CODE
-        response
-      when SERVICE_ERROR_RETURN_CODE
-        raise ServiceError, 'Service could not handle the request'
-      else
-        raise InvalidData.new('Invalid input data', response.return_code)
-      end
+      response.return_code == SERVICE_ERROR_RETURN_CODE ? raise(ServiceError) : response
     end
 
     private
--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -21,44 +21,15 @@ RSpec.describe IbanCalculator::Client do
       expect(adapter).to have_received(:call).with(operation, message: hash_including(user: 'user', password: 'password'))
     end
 
-    context 'valid response' do
-      it 'returns a response object' do
-        expect(subject.(operation, valid_payload)).to be_a IbanCalculator::Response
-      end
-    end
-
-    context 'invalid response' do
-      let(:response) { double(body: { calculate_iban_response: { return: valid_payload.merge(return_code: '128') } }) }
-
-      xit 'logs a message' do
-        subject.call({}) rescue IbanCalculator::InvalidData
-        expect(subject.logger).to have_received(:info).with(/iban check invalid/)
-      end
-
-      it 'fails with invalid data exception' do
-        expect { subject.call(:calculate_iban)}.to raise_exception(IbanCalculator::InvalidData)
-      end
+    it 'returns a response object' do
+      expect(subject.(operation, valid_payload)).to be_a IbanCalculator::Response
     end
 
     context 'server error response' do
       let(:response) { double(body: { calculate_iban_response: { return: valid_payload.merge(return_code: '65536') } }) }
 
-      xit 'logs a message' do
-        subject.call({}) rescue IbanCalculator::ServiceError
-        expect(subject.logger).to have_received(:info).with(/iban check failed/)
-      end
-
       it 'fails with service error exception' do
         expect{ subject.call(:calculate_iban) }.to raise_exception(IbanCalculator::ServiceError)
-      end
-    end
-
-    context 'probably valid response' do
-      let(:response) { double(body: { calculate_iban_response: { return: valid_payload.merge(return_code: '32') } }) }
-
-      xit 'logs a message' do
-        subject.call({}) rescue IbanCalculator::InvalidData
-        expect(subject.logger).to have_received(:info).with(/needs manual check/)
       end
     end
   end
