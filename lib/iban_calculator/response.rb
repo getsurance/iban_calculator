d3d40f821e7f248f4fe482759709dca62221134d
Christian Georgii <cgeorgii@gmail.com>
2017-12-14 17:07:25 +0100

    response knows its validness

--- lib/iban_calculator/response.rb
+++ lib/iban_calculator/response.rb
@@ -2,6 +2,11 @@ require 'dry-struct'
 
 module IbanCalculator
   class Response < Dry::Struct::Value
+    CONCLUSIVENESS_THRESHOLD = 32
+    CORRECTNESS_THRESHOLD = 127
+
+    constructor_type :schema
+
     attribute :result, Dry::Types['strict.string']
     attribute :return_code, Dry::Types['coercible.int']
     attribute :iban, Dry::Types['strict.string'].optional
@@ -9,5 +14,13 @@ module IbanCalculator
     attribute :bank_code, Dry::Types['strict.string'].optional
     attribute :bank, Dry::Types['strict.string'].optional
     attribute :account_number, Dry::Types['strict.string'].optional
+
+    def valid?
+      return_code < CORRECTNESS_THRESHOLD && result == 'passed'
+    end
+
+    def conclusive?
+      return_code < CONCLUSIVENESS_THRESHOLD
+    end
   end
 end
